eb0047c157562f7e0231891f3ed2886f340e7b4f
ecsnail <ecsnail@gmail.com>
2013-10-01 23:09:52 +0800

    create my personal project

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.opencart.me/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.opencart.me/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/opencart/system/');
+define('DIR_DATABASE', '/var/www/opencart/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/opencart/image/');
+define('DIR_CACHE', '/var/www/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart/download/');
+define('DIR_LOGS', '/var/www/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '360weboy');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
